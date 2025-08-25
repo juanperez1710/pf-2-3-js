567ec0d95d797e236371090d6c8a7d3bd7d4cb93
juanperez1710 <junpemarc@gmail.com>
2025-08-25 12:20:40 -0600

    se agrega código

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,4 +1,18 @@
 export function fizzbuzz(){
-  //tu código va aqui
   
+for (let i=1 ; i<=100 ; i++){
+  if (i % 3===0 && i % 5===0 ) {
+    console.log("Fizzbuzz")
+  }
+else if  (i % 3===0 && i % 5!==0){
+  console.log("Fizz)")
+}
+else if  (i % 3!==0 && i % 5===0){
+  console.log("Buzz)")
+}
+else {
+  console.log(i)
+}
+}
+
 }
